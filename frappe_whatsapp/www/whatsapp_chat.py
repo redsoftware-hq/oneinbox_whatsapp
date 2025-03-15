2157c7f1e9d12ce00f1e850fb44fff065f3bd76f
sandip_pandit <sandip.pandit@redsoftware.in>
2025-03-15 14:05:35 +0530

    fix:made some changes in vite config to load boot correctly

--- frappe_whatsapp/www/whatsapp_chat.py
+++ frappe_whatsapp/www/whatsapp_chat.py
@@ -10,7 +10,9 @@ from frappe.utils.telemetry import capture
 no_cache = 1
 
 
-def get_context():
+def get_context(context):
+	csrf_token = frappe.sessions.get_csrf_token()
+
 	frappe.db.commit()
 	context = frappe._dict()
 	context.boot = get_boot()
